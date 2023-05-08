2562aac6637c7e1d6857c88463a1a9a8b2b70910
Sam Merham <sam@smerham.com>
2023-05-08 14:22:25 -0700

    isRef, isReactive

--- src/hooks/number.js
+++ src/hooks/number.js
@@ -1,7 +1,15 @@
-import {ref, computed} from 'vue';
-
+import { ref, computed, isRef, isReactive, reactive } from 'vue';
+// isRef : can be use to check if a variable is reactive that was created with ref func 
+// isRef : can be use to check if a variable is reactive that was created with reactive func 
 export const useNumber = ()=>{
     let num = ref(0);
+    const accounts = reactive({
+      checking: 234,
+      savings: 123,
+    });
+
+    console.log(isRef(num));
+    console.log(isReactive(accounts));
     function increment() {
       num.value++;
     };
