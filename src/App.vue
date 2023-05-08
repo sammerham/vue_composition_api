4934258f1ccaef7d34eda037e56e4999c344c660
Sam Merham <sam@smerham.com>
2023-05-08 08:23:00 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <p>{{num}}</p>
+  <button type="button" @click.prevent="increment">Click Me</button>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+    console.log('num--->>', num);
+    function increment () {
+      num.value++;
+    };
+    return {
+      num,
+      increment,
+    };
+  },
+};
+</script>
