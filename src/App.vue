e64d7a5d56ddec302fe47e68f294f00abcb41cd1
Sam Merham <sam@smerham.com>
2023-05-08 09:25:56 -0700

    added watchers

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,51 @@
+/* https://vuejs.org/guide/extras/composition-api-faq.html ref: primitive values(num, str,
+boolean ), access value using .value reactive: objects, access value directly on obj
+toRefs: converts obj props into separate reactive references watchEffect: allows us to
+watch depenceies in the componennts */
+
 <template>
   <div>
-  <p>{{num}}</p>
-  <button type="button" @click.prevent="increment">Click Me</button>
+    <p>{{ num }}</p>
+    <button type="button" @click.prevent="increment">Click Me</button>
+    <p>{{ name }}</p>
+    <p>
+      <input type="text" v-model="phrase" />
+    </p>
+    <p>{{reversedPhrase}}</p>
   </div>
 </template>
 
 <script>
-import {ref} from "vue";
+import { ref, reactive, toRefs, watchEffect, watch } from "vue";
 export default {
   name: "App",
   setup() {
     let num = ref(0);
-    console.log('num--->>', num);
-    function increment () {
+    function increment() {
       num.value++;
-    };
+    }
+    const user = reactive({
+      name: "Tom",
+      age: 30,
+    });
+    setTimeout(() => {
+      user.name = "Hardy";
+    }, 3000);
+    const phrase = ref("");
+    const reversedPhrase = ref("");
+
+    // watchEffect(() => {
+    //   reversedPhrase.value = phrase.value.split("").reverse().join("");
+    // });
+    watch([phrase], ([newVal, oldVal]) => {
+      reversedPhrase.value = phrase.value.split("").reverse().join("");
+    });
     return {
       num,
       increment,
+      ...toRefs(user),
+      phrase,
+      reversedPhrase,
     };
   },
 };
